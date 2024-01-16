d087a961d3ccc4fa10ab5bf0c2bb5486ca1a8ba4
Olesya Dorofeeva <lesjok@inbox.ru>
2024-01-16 22:06:42 +0300

    feat: add react-router-dom

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import reportWebVitals from './reportWebVitals';
+import AppRouter from './router/AppRouter';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -12,7 +12,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
       <Provider store={store}>
-          <App />
+          <React.StrictMode>
+              <AppRouter />
+          </React.StrictMode>
       </Provider>
   </React.StrictMode>
 );
---
+++ src/router/AppRouter.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Main from '../pages/MainPage/MainPage';
+
+const AppRouter: React.FC = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Main />} />
+            </Routes>
+        </BrowserRouter>
+    );
+};
+
+export default AppRouter;
